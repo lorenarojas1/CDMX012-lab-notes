671868355b9aa860135444b1b31a67caf6b8b744
Lorenarojas1 <lorena.rojas080912@gmail.com>
2022-04-12 23:00:12 -0500

    Create react app

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOLso3Nram1heAfO8GVlmEJAeyo-XPG-c",
+    authDomain: "labnotes-7060b.firebaseapp.com",
+    projectId: "labnotes-7060b",
+    storageBucket: "labnotes-7060b.appspot.com",
+    messagingSenderId: "51562476133",
+    appId: "1:51562476133:web:2c8e81c29a4fa550dccc8c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
